35edebf2bbd031c6d7189ee2f3dccc14c164f830
itunuelijah <itunuelijah@gmail.com>
2022-07-01 20:36:16 -0700

    if else

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main -> assign a random number to the variable n each time it is executed
+ */
+int main(void)
+{
+	int ch;
+	for (ch = 997; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10); /* this is an ascii code for new line*/
+
+	return (0);
+}
